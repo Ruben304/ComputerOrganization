cf1c7940a5c74398139685644b6df393c8a32bea
clairecropper <90360399+clairecropper@users.noreply.github.com>
2023-02-22 16:01:56 -0500

    Add files via upload

---
+++ Lab4/FA_strD_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/22/2023 03:57:04 PM
+// Design Name: 
+// Module Name: FA_strD_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FA_strD_tb(
+
+    );
+endmodule
