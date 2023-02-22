6891f092962655233f23fac2eb485eb3a965a417
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2023-02-22 17:31:26 -0500

    Update sixtyfour_bitadder.v

--- Lab4/sixtyfour_bitadder.v
+++ Lab4/sixtyfour_bitadder.v
@@ -29,6 +29,6 @@ module sixtyfour_bitadder(a, b, c_in, c_out, sum);
     wire fa1_out;
     
     thirtytwo_bitadder SFA1(.c_out(fa1_out), .sum(sum[31:0]), .a(a[31:0]), .b(b[31:0]), .c_in(c_in));
-    thirtytwo_bitadder SFA2(.c_out(c_out), .sum(sum[63:31]), .a(a[63:31]), .b(b[63:31]), .c_in(fa1_out));
+    thirtytwo_bitadder SFA2(.c_out(c_out), .sum(sum[63:32]), .a(a[63:32]), .b(b[63:32]), .c_in(fa1_out));
     
 endmodule
