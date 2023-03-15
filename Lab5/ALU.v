c877c6ac446faf7dc1a266f5ff942be152889773
clairecropper <90360399+clairecropper@users.noreply.github.com>
2023-03-15 18:18:50 -0400

    Update ALU.v

--- Lab5/ALU.v
+++ Lab5/ALU.v
@@ -19,16 +19,15 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module ALU #(parameter N = 32)(in1_val, in2_val, mux_in, c_in, out1_val, c_out, out2_val, clk);
+module ALU #(parameter N = 32)(in1_val, in2_val, mux_in, c_in, out1_val, c_out, clk);
 
 	input [N-1:0] in1_val, in2_val;
 	input [2:0] mux_in;
 	input c_in, clk;
-	output [N-1:0] out1_val;
+	output reg [N-1:0] out1_val;
 	output c_out;
-	output reg [N-1:0] out2_val;
 	
-	
+
 	wire [N-1:0] mov_out;
 	wire [N-1:0] not_out;
 	wire [N-1:0] add_out;
@@ -60,31 +59,33 @@ module ALU #(parameter N = 32)(in1_val, in2_val, mux_in, c_in, out1_val, c_out, 
 		always @(*) begin
 			case(mux_in)
 				3'b000: // mov
-			         out2_val[N-1:0] = mov_out[N-1:0];
+			         out1_val[N-1:0] = mov_out[N-1:0];
 				3'b001:	// not
-				    out2_val[N-1:0] = not_out[N-1:0];
+				    out1_val[N-1:0] = not_out[N-1:0];
 				3'b010: // add
 				    begin
-				        out2_val[N-1:0] = add_out[N-1:0];
+				        out1_val[N-1:0] = add_out[N-1:0];
 				        c_out_output = c_out_add;
 				    end    
 				3'b011: // nor
-				    out2_val[N-1:0] = nor_out[N-1:0];
+				    out1_val[N-1:0] = nor_out[N-1:0];
 				3'b100: // sub
 				    begin
-				        out2_val[N-1:0] = sub_out[N-1:0];
+				        out1_val[N-1:0] = sub_out[N-1:0];
 				        c_out_output = c_out_sub;
 				    end
 				3'b101: // nand
-				    out2_val[N-1:0] = nand_out[N-1:0]; 
+				    out1_val[N-1:0] = nand_out[N-1:0]; 
 				3'b110: // and
-				    out2_val[N-1:0] = and_out[N-1:0];
+				    out1_val[N-1:0] = and_out[N-1:0];
 				3'b111: // slt
 				    c_out_output = c_out_slt;
 		    endcase
         end		
 
-    NBit_Register #(.N(N)) regALU (.out0(out1_val[N-1:0]), .in0(out2_val[N-1:0]), .clk(clk));
+    assign c_out = c_out_output;
+
+    NBit_Register #(.N(N)) regALU (.out0(out1_val[N-1:0]), .in0(output1[N-1:0]), .clk(clk));
 
     dff dffALU (.q(c_out), .d(c_out), .clk(clk));
 
