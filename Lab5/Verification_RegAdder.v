a9b2c24e238758bf1058731fbb03c7a7cf44beaf
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2023-03-15 16:35:53 -0400

    final version

--- Lab5/Verification_RegAdder.v
+++ Lab5/Verification_RegAdder.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ns
 
 module Verification_RegAdder(c_out,sum, a,b,c_in, select,clk);
-parameter n=8;
+parameter n=32;
 input [n-1:0] a;
 input [n-1:0] b;
 input c_in;
@@ -16,7 +16,8 @@ wire c_out_temp;
 assign {c_out_temp, sum_temp}= a+b+c_in;
 
 always @(posedge clk)begin
-    c_out<=c_out_temp;
-    sum<=(select)?~a : sum_temp;
+c_out<=c_out_temp;
+sum<=(select)?~a : sum_temp;
 end
 endmodule
+
