29c5b2be7f33704f4392e3c39150186cfba1d53d
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2023-03-15 16:27:59 -0400

    final version

--- Lab5/NBit_Adder.v
+++ Lab5/NBit_Adder.v
@@ -1,8 +1,6 @@
 
-
 module Nbit_adder(c_out, sum,a,b,c_in);
-
-parameter n=8;
+parameter n=32;
 input [n-1:0] a;
 input[n-1:0] b;
 input c_in;
@@ -15,8 +13,7 @@ assign c_out=carry[n];
 genvar i;
 generate 
 for(i=0; i<n; i=i+1) begin 
-    FA_str Adder1bit (carry[i+1], sum[i],a[i],b[i],carry[i]);
+FA_str Adder1bit (carry[i+1], sum[i],a[i],b[i],carry[i]);
 end 
 endgenerate
 endmodule 
-
