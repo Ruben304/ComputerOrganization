7213e7c5ea51af5376cb6ddf18a2e363d90f912c
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2023-03-21 23:59:43 -0400

    added in/out puts

--- Lab6/S2_Register.v
+++ Lab6/S2_Register.v
@@ -25,10 +25,16 @@ module S2_Register(
 	input [31:0] Reg_ReadData2,
 	input [4:0] S1_WriteSelect,
 	input S1_WriteEnable,
+	input [15:0] S1_imm,
+	input S1_datasrc,
+	input [2:0] S1_aluop,
 	output reg [31:0] S2_ReadData1,
 	output reg [31:0] S2_ReadData2,
 	output reg [4:0] S2_WriteSelect,
-	output reg S2_WriteEnable
+	output reg S2_WriteEnable,
+	output reg [15:0] S2_IMM,
+	output reg S2_DataSrc,
+	output reg [2:0] S2_ALUOp
    );
 
 	always@(posedge clk)
@@ -39,6 +45,9 @@ module S2_Register(
 			S2_ReadData2	<= 32'd0;
 			S2_WriteSelect <= 5'd0;
 			S2_WriteEnable <= 1'b0;
+			S2_IMM <= 16'b0;
+			S2_DataSrc <= 1'b0;
+			S2_ALUOp <= 3'b0;
 			end
 		else
 			begin
@@ -46,6 +55,9 @@ module S2_Register(
 			S2_ReadData2	<= Reg_ReadData2;
 			S2_WriteSelect <= S1_WriteSelect;
 			S2_WriteEnable <= S1_WriteEnable;
+			S2_IMM <= S1_imm;
+			S2_DataSrc <= S1_datasrc;
+			S2_ALUOp <= S1_aluop;
 			end
 		end
 
