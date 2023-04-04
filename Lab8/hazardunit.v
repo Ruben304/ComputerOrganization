b39dd63aa19832b1b7e95e23bcb8d1a8bf1f151f
clairecropper <90360399+clairecropper@users.noreply.github.com>
2023-04-04 19:20:47 -0400

    Update hazardunit.v
    had procedural assignment in an always block

--- Lab8/hazardunit.v
+++ Lab8/hazardunit.v
@@ -5,17 +5,17 @@ module hazardunit(IFIDWrite,PCWrite,HazardMux, IDEX_Rs, IDEX_Rt, IDEX_Rd, EXMEM_
 	
     always @(*) begin
         if (inWex && (IDEX_Rd == IDEX_Rs || IDEX_Rd == IDEX_Rt) && IDEX_Rd != 0) begin // 1 ahead hazards
-		  assign IFIDWrite = 0; 
-		  assign PCWrite = 0;
-		  assign HazardMux = 1;
+		  IFIDWrite = 0; 
+		  PCWrite = 0;
+		  HazardMux = 1;
         end else if (inWmem && (EXMEM_Rd == IDEX_Rs || EXMEM_Rd == IDEX_Rt) && EXMEM_Rd != 0) begin // 2 ahead hazards
           assign IFIDWrite = 0; 
-		  assign PCWrite = 0;
-		  assign HazardMux = 1;
+		  PCWrite = 0;
+		  HazardMux = 1;
 		end else begin  // no hazard detected
-		  assign IFIDWrite = 1; 
-		  assign PCWrite = 1;
-		  assign HazardMux = 0;
+		  IFIDWrite = 1; 
+		  PCWrite = 1;
+		  HazardMux = 0;
 	    end
      end
             
