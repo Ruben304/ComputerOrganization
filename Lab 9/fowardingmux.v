bbfb7815e19b1f3cc59896af337610c75a116414
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2023-04-26 16:31:46 -0400

    changed a bit

--- Lab 9/fowardingmux.v
+++ Lab 9/fowardingmux.v
@@ -25,7 +25,7 @@ module forwardingmux(EX_MEM_Rd, MEM_WB_Rd, ID_EX_Rs, ID_EX_Rt, EX_MEM_RegWrite, 
     input EX_MEM_RegWrite, MEM_WB_RegWrite, EX_MEM_MemWrite;
     output [1:0] forwardA,forwardB;
     
-    assign forwardA = (EX_MEM_RegWrite && (EX_MEM_Rd == ID_EX_Rs)) ? 2'b10 : (MEM_WB_RegWrite && (MEM_WB_Rd == ID_EX_Rs)) ? 2'b01 : 2'b00;
-    assign forwardB = ((EX_MEM_MemWrite || EX_MEM_RegWrite) && (EX_MEM_Rd == ID_EX_Rt)) ? 2'b10 : (MEM_WB_RegWrite && (MEM_WB_Rd == ID_EX_Rt)) ? 2'b01 : 2'b00;
+    assign forwardA = ((EX_MEM_Rd == ID_EX_Rs) && EX_MEM_RegWrite) ? 2'b10 : ((MEM_WB_Rd == ID_EX_Rs) && MEM_WB_RegWrite) ? 2'b01 : 2'b00;
+    assign forwardB = ((EX_MEM_Rd == ID_EX_Rt) && (EX_MEM_MemWrite || EX_MEM_RegWrite)) ? 2'b10 : ((MEM_WB_Rd == ID_EX_Rt) && MEM_WB_RegWrite) ? 2'b01 : 2'b00;
     
 endmodule
